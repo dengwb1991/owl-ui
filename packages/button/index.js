e212055c86492696c119571d9ce083c7833c2803
dengwb <dengwenbin@wecash.net>
2018-12-15 15:53:19 +0800

    del

--- packages/button/index.js
+++
@@ -1,6 +0,0 @@
-import OwlButton from './src/Main'
-
-OwlButton.install = function (Vue) {
-  Vue.component(OwlButton.name, OwlButton)
-}
-export default OwlButton
--- packages/tabs/index.js
+++
@@ -1,6 +0,0 @@
-import OwlTabBar from './src/Main'
-
-OwlTabBar.install = function (Vue) {
-  Vue.component(OwlTabBar.name, OwlTabBar)
-}
-export default OwlTabBar
