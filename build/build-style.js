25172e9c0ee0065afed34e65cb00d393334f0831
dengwb <dengwenbin@wecash.net>
2018-12-19 11:14:44 +0800

    add pxtorem to gulp build

--- build/build-style.js
+++ build/build-style.js
@@ -3,26 +3,54 @@ const cleanCSS = require('gulp-clean-css')
 const less = require('gulp-less')
 const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer')
+const postcss = require('gulp-postcss')
+const pxtorem = require('postcss-pxtorem')
+
+let ap = {
+  browsers: ['last 2 versions', 'ie > 8']
+}
+
+let processors = [
+  pxtorem({
+    rootValue: 75,
+    propList: ["*"]
+  })
+]
 
 gulp.task('owl-ui', function () {
   gulp.src('../src/styles/index.less')
     .pipe(less())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'ie > 8']
-    }))
+    .pipe(autoprefixer(ap))
+    .pipe(postcss(processors))
     .pipe(cleanCSS())
     .pipe(rename('owl-ui.css'))
+    .pipe(gulp.dest('../dist/styles'))
+})
+
+gulp.task('owl-ui-px', function () {
+  gulp.src('../src/styles/index.less')
+    .pipe(less())
+    .pipe(autoprefixer(ap))
+    .pipe(cleanCSS())
+    .pipe(rename('owl-ui-px.css'))
     .pipe(gulp.dest('../dist/styles'))
 })
 
 gulp.task('packages', function () {
   gulp.src('../src/styles/packages/*.less')
     .pipe(less())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'ie > 8']
-    }))
+    .pipe(autoprefixer(ap))
+    .pipe(postcss(processors))
     .pipe(cleanCSS())
     .pipe(gulp.dest('../dist/styles/packages'))
 })
 
-gulp.task('default', ['packages', 'owl-ui'])
+gulp.task('packages-px', function () {
+  gulp.src('../src/styles/packages/*.less')
+    .pipe(less())
+    .pipe(autoprefixer(ap))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('../dist/styles/packages-px'))
+})
+
+gulp.task('default', ['owl-ui', 'owl-ui-px', 'packages', 'packages-px'])
