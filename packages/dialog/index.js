b566b5739a28cad4f6c39022ea2d5d0ff91ff3de
dengwb <dengwenbin@wecash.net>
2019-03-06 18:31:31 +0800

    modify style prefix-cls

---
+++ packages/dialog/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import OwlDialog from './src/main'
+import CreateAPI from 'create-api'
+
+OwlDialog.install = function (Vue) {
+  Vue.component(OwlDialog.name, OwlDialog)
+}
+
+export default OwlDialog
---
+++ packages/dialog/src/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="owl-dialog">
+    <transition name="fade">
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OwlDialog'
+}
+</script>
