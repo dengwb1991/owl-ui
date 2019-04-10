a9a8d0cfc89476ecf8545f8fda0bbb5e5e937958
dengwb <dengwenbin@wecash.net>
2019-04-10 15:18:25 +0800

    del: icon component

--- packages/icon/index.js
+++
@@ -1,6 +0,0 @@
-import OwlIcon from './src/main'
-
-OwlIcon.install = function (Vue) {
-  Vue.component(OwlIcon.name, OwlIcon)
-}
-export default OwlIcon
--- packages/icon/src/main.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <i class="owl-iconfont-close"></i>
-</template>
-
-<script>
-export default {
-  name: 'OwlIcon'
-}
-</script>
