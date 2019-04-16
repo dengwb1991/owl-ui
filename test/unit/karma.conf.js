3eeff57f389d6450e28c83caca4a16d4eb04c979
dengwb <dengwenbin@wecash.net>
2019-04-16 10:35:44 +0800

    feat: browsers phantomjs => Chrome

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -7,12 +7,14 @@ var webpackConfig = require('../../build/webpack.test.config')
 
 module.exports = function karmaConfig (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
@@ -28,6 +30,11 @@ module.exports = function karmaConfig (config) {
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
       ]
+    },
+    client: {
+      mocha: {
+        timeout: 4000
+      }
     }
   })
 }
