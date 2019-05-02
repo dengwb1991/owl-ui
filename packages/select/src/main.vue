09087c65b0be4731f025bc6ffe8d8a3417d0cd53
dengwb <dengwenbin@wecash.net>
2019-05-02 21:30:02 +0800

    feat: select package

--- packages/select/src/main.vue
+++ packages/select/src/main.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="owl-select">
+    <owl-input v-model="selectValue"/>
   </div>
 </template>
 
 <script>
+import OwlInput from '../../input/src/main.vue'
 export default {
-  name: 'OwlSelect'
+  name: 'OwlSelect',
+  components: {
+    OwlInput
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: null
+    }
+  },
+  data () {
+    return {
+      selectValue: this.value
+    }
+  }
 }
 </script>
