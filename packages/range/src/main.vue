0b2914ef85fd457f97b36ac86ed5427e7b79d856
dengwb <dengwenbin@wecash.net>
2019-06-04 20:18:48 +0800

    feat: add range code

--- packages/range/src/main.vue
+++ packages/range/src/main.vue
@@ -10,6 +10,85 @@
 
 <script>
 export default {
-  name: 'OwlRange'
+  name: 'OwlRange',
+  props: {
+    value: [Number, String],
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    min: {
+      type: [String, Number],
+      default: 0
+    },
+    max: {
+      type: [String, Number],
+      default: 100
+    },
+    step: {
+      type: [String, Number],
+      default: 1
+    }
+  },
+  data () {
+    return {
+      actualValue: null
+    }
+  },
+  computed: {
+    _min () {
+      return +this.min
+    },
+    _max () {
+      return +this.max
+    },
+    _step () {
+      return +this.step
+    },
+    valuePercent () {
+      return (this.actualValue - this._min) / (this._max - this._min) * 100
+    }
+  },
+  methods: {
+    round (value, min, max, step) {
+      if (value <= min) {
+        return min
+      }
+
+      const roundedMax = Math.floor((max - min) / step) * step + min
+      if (value >= roundedMax) {
+        return roundedMax
+      }
+
+      const normalize = (value - min) / step
+      const decimal = Math.floor(normalize)
+      const fraction = normalize - decimal
+
+      if (fraction === 0) return value
+
+      if (fraction < 0.5) {
+        return step * decimal + min
+      } else {
+        return step * (decimal + 1) + min
+      }
+    },
+    getVal (val) {
+      this.round(val, this._min, this._max, this._step)
+    }
+  },
+  watch: {
+    value: {
+      handler (val) {
+        this.actualValue = this.getVal(val)
+      },
+      immediate: true
+    },
+    min () {
+      this.actualValue = this.getVal(this.actualValue)
+    },
+    max () {
+      this.actualValue = this.getVal(this.actualValue)
+    }
+  }
 }
 </script>
