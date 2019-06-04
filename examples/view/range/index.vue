273fa8c9ec8a77e9fc6c4bd8fc8aafca6fe04ad6
dengwb <dengwenbin@wecash.net>
2019-06-04 16:46:42 +0800

    feat: add range less

---
+++ examples/view/range/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <owl-page>
+    <owl-range/>
+  </owl-page>
+</template>
--- packages/range/src/main.vue
+++ packages/range/src/main.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="owl-range">
+    <div class="owl-range-wrap">
+      <input type="hidden" value="val"/>
+      <div class="owl-range-bar" style="width: 30%"/>
+      <div class="owl-range-button-wrap" style="left: 30%"></div>
+    </div>
   </div>
 </template>
 
